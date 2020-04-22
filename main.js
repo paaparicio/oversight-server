c4ae73a715fed09af581217e9eeeda80da25bb81
Pablo <pablo.aprc@gmail.com>
2020-04-22 13:32:36 +0200

    Create server https

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const WebSocket = require('ws');
+const https = require('https');
+
 const Rooms = require('./Server/Classes/Channels');
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+const server = https.createServer().listen(process.env.PORT || 8080);
+
+const wss = new WebSocket.Server({ server });
 
 const channels = new Rooms();
 
