6770842754f79861614943901de8e4bc9c9175ce
Pablo <pablo.aprc@gmail.com>
2020-04-22 12:42:24 +0200

    My first commit

---
+++ Server/Classes/Channels.js
@@ -0,0 +1,116 @@
+const shortid = require('shortid');
+
+module.exports = class Channels {
+    constructor() {
+        this.CHANNEL_USER = {};
+        this.CHANNEL_NAME = "";
+        this.MAX_USERS = 2;
+
+        this.CHANNELS_ARRAY = [{name: "", users: [], connected: 0}];
+
+        this.ERROR_MESSAGES = {
+            session_full: "This game is actually used by another player.",
+            session_exist: "Your session doesn't exist.",
+            game_disconnect: "The game is disconnected."
+        };
+    }
+
+    setVariablesUser(client) {
+        this.CHANNEL_USER = client;
+        this.CHANNEL_USER.id = client.header.id;
+        this.CHANNEL_NAME = client.header.channel;
+        this.CHANNEL_DEVICE = client.header.device;
+    }
+
+    generateID() {
+        return Math.floor(Math.random() * 1000);
+    }
+
+    isChannelExist(name) {
+        return this.CHANNELS_ARRAY.some(channel => channel.name === name);
+    }
+    isChannelIsFull(name) {
+       const filter =  this.CHANNELS_ARRAY.filter(channel => channel.name === name);
+       return filter[0].connected < this.MAX_USERS;
+    }
+
+    createUsersChannel() {
+       this.CHANNELS_ARRAY.push({
+           name: this.CHANNEL_NAME,
+           users: [this.CHANNEL_USER],
+           connected: 1
+       });
+    }
+    addUserToChannel() {
+
+        if(this.isChannelExist(this.CHANNEL_NAME)) {
+            if(this.isChannelIsFull(this.CHANNEL_NAME)) {
+                this.CHANNELS_ARRAY.forEach(channel => {
+                    if(channel.name === this.CHANNEL_NAME) {
+                        channel.users.push(this.CHANNEL_USER);
+                        channel.connected = channel.users.length;
+                    }
+                });
+            } else {
+                this.CHANNEL_USER.send(JSON.stringify({error: this.ERROR_MESSAGES.session_full}));
+                this.CHANNEL_USER.close();
+            }
+        } else {
+            this.CHANNEL_USER.send(JSON.stringify({error: this.ERROR_MESSAGES.session_exist}));
+            this.CHANNEL_USER.close();
+        }
+    }
+
+    sendMessageChannel(client, message) {
+        let filter = this.CHANNELS_ARRAY.filter(channel => channel.name === client.channel);
+
+        filter.forEach(channel => {
+            channel.users.forEach(user => user.send(message))
+        })
+    }
+
+    connectUser(client) {
+        this.setVariablesUser(client);
+
+        if(this.CHANNEL_DEVICE === "UNITY") {
+            let name = shortid.generate();
+
+            client.header.channel = name;
+
+            this.CHANNEL_USER.send(name);
+
+            this.setVariablesUser(client);
+            this.createUsersChannel();
+        } else {
+            this.addUserToChannel();
+        }
+
+        console.log(this.CHANNELS_ARRAY)
+    }
+
+    disconnectUser(client) {
+        this.setVariablesUser(client);
+
+        this.CHANNELS_ARRAY.forEach((channel, x) => {
+            if(channel.connected > 0) {
+                channel.users.forEach((user, y) => {
+                    if(user.id === this.CHANNEL_USER.id) {
+                        this.CHANNELS_ARRAY[x]['users'].splice(y, 1);
+                    }
+                });
+
+                if(this.CHANNEL_DEVICE === "UNITY") {
+                    if(channel.name === this.CHANNEL_NAME) {
+                        channel.users.forEach(user => {
+                            user.send(JSON.stringify({error: this.ERROR_MESSAGES.game_disconnect}));
+                            user.close()
+                        });
+                    }
+                }
+            }
+
+            channel.connected = channel.users.length;
+            (channel.name !== "" && channel.connected === 0) && this.CHANNELS_ARRAY.splice(x, 1);
+        });
+    }
+};
---
+++ Server/main.js
@@ -0,0 +1,45 @@
+const WebSocket = require('ws');
+const Rooms = require('./Classes/Channels');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+const channels = new Rooms();
+
+wss.on('connection', function connection(ws, req) {
+    ws.header = splitDataByURL(req.url);
+    ws.header.id = channels.generateID();
+
+    console.log('Server - OnOpen', "user: "+ ws.header.id);
+
+    channels.connectUser(ws);
+
+    ws.on('message', function incoming(data) {
+        console.log('Server - OnMessage', "user: "+ ws.header.id);
+        console.log(channels.CHANNELS_ARRAY);
+
+        channels.sendMessageChannel(ws.header, data)
+    });
+
+    ws.on('close', () => {
+        channels.disconnectUser(ws);
+
+        console.log('Server - OnClose', "user: "+ ws.header.id);
+        console.log(channels.CHANNELS_ARRAY);
+    });
+});
+
+function splitDataByURL(data) {
+    let header = {};
+    let url = data.substring(2);
+    let separateByDatas = url.split('&');
+
+    separateByDatas.forEach(data => {
+        let separateByKeys = data.split("=");
+
+        separateByKeys.forEach((key, i) => {
+            !(i % 2) && (header[key] = separateByKeys[i + 1]);
+        })
+    })
+
+    return header;
+}
