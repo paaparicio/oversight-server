0f04d741eb9a4f09733c02e9deb799c21002ee8e
Pablo <pablo.aprc@gmail.com>
2020-04-22 16:26:10 +0200

    Test 5: socket.io

--- socket.js
+++ socket.js
@@ -1,6 +1,10 @@
 const app = require('express')();
-const server = require('http').createServer(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', client => {
     console.log('Client connected');
@@ -8,6 +12,6 @@ io.on('connection', client => {
     client.on('disconnect', () => {
         console.log('Client disconnect');
     })
-})
+});
 
 server.listen(process.env.PORT || 8080);
