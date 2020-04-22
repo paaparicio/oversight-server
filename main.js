6731ed08c6dbf4431429c316f8a1ffe0782fd97c
Pablo <pablo.aprc@gmail.com>
2020-04-22 13:35:12 +0200

    Update 1: server https

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const https = require('https');
 
 const Rooms = require('./Server/Classes/Channels');
 
-const server = https.createServer().listen(process.env.PORT || 8080);
-
+const server = https.createServer();
 const wss = new WebSocket.Server({ server });
 
 const channels = new Rooms();
@@ -33,6 +32,8 @@ wss.on('connection', function connection(ws, req) {
         console.log(channels.CHANNELS_ARRAY);
     });
 });
+
+server.listen(process.env.PORT || 8080);
 
 function splitDataByURL(data) {
     let header = {};
